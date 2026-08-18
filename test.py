f09652f7365c436dccfddf68ace34dc08da04ae8
EOtswaba <eotswabakivuvu@gmail.com>
2026-08-18 11:13:13 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,6 @@
+#Petit programme de calculatrice
+
+nombre1 = int(input("Entrer un nombre entier"))
+nombre2 = int(input("Entrer un deux nombre entier"))
+
+print("La somme de ces nombres est :", nombre1+nombre2)
